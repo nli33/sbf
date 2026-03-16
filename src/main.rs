22bc76ef8594c4a4482b102c1b1470c2fcc18a8b
nli33 <domesticatedhuman9999@gmail.com>
2026-03-16 15:18:07 -0400

    improve cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
+use std::env;
 use std::io::{stdin, stdout};
+use std::process;
+
 mod bf;
 
-
 fn main() {
-    match bf::Interpreter::new(
-        "test_prog",
+    let mut args = env::args();
+    
+    let program = args.next().unwrap();
+    let file = match args.next() {
+        Some(f) => f,
+        None => {
+            eprintln!("usage: {} <program.bf>", program);
+            process::exit(1);
+        }
+    };
+
+    // one arg only
+    if args.next().is_some() {
+        eprintln!("usage: {} <program.bf>", program);
+        process::exit(1);
+    }
+
+    let mut interpreter = match bf::Interpreter::new(
+        &file,
         stdin(),
         stdout(),
     ) {
-        Ok(mut interpreter) => {
-            if let Err(e) = interpreter.execute() {
-                eprintln!("{}", e);                
-            }
-        },
-        Err(e) => eprintln!("{}", e),
+        Ok(i) => i,
+        Err(e) => {
+            eprintln!("error loading program: {}", e);
+            process::exit(1);
+        }
     };
+
+    if let Err(e) = interpreter.execute() {
+        eprintln!("runtime error: {}", e);
+        process::exit(1);
+    }
 }
